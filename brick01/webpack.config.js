93fe22dc1c0750fe3792d09ccf24878dfe89d90c
sea13502 <sea13502@163.com>
2016-12-05 15:02:09 +0800

    单入口的写法

--- brick01/webpack.config.js
+++ brick01/webpack.config.js
@@ -1,17 +1,17 @@
 //单入口
-// module.exports = {
-//   entry: './main1.js',
-//   output: {
-//     filename: 'bundle.js'
-//   }
-// };
-//多入口
 module.exports = {
-  entry: {
-    bundle1: './main1.js',
-    bundle2: './main2.js'
-  },
+  entry: './main1.js',
   output: {
-    filename: '[name].js'
+    filename: 'bundle.js'
   }
 };
+//多入口
+// module.exports = {
+//   entry: {
+//     bundle1: './main1.js',
+//     bundle2: './main2.js'
+//   },
+//   output: {
+//     filename: '[name].js'
+//   }
+// };
