e605dee31c823fd45c114ca3985a19c9061ed392
sea13502 <sea13502@163.com>
2016-12-06 15:21:19 +0800

    增加openbrowserwebpackplugin

--- brick04/webpack.config.js
+++ brick04/webpack.config.js
@@ -1,5 +1,7 @@
 //介绍htmlwebpack的基本使用，和js文件名［hash］和使用，还有简单说下［name］［id］的用处
+//webpack－dev－server 和 open－browser-webpack-plugin
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
   entry: './main.js',
   output: {
@@ -10,6 +12,9 @@ module.exports = {
       title: 'Webpack-demos',
       filename: 'index.html',
       template: 'temp.html'
+    }),
+    new OpenBrowserPlugin({
+      url:'http://localhost:8889'
     })
   ]
 };
