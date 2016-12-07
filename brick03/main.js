cb9b76768e856ca0447e8c7a44ef26369b6d174f
sea13502 <sea13502@163.com>
2016-12-07 08:58:10 +0800

    增加string－loader

--- brick03/main.js
+++ brick03/main.js
@@ -5,3 +5,8 @@ document.body.appendChild(img1);
 var img2 = document.createElement("img");
 img2.src = require("./big.png");
 document.body.appendChild(img2);
+
+var html = require('./part.html');
+var div = document.createElement("div");
+div.innerHTML = html;
+document.body.appendChild( div );
--- brick03/webpack.config.js
+++ brick03/webpack.config.js
@@ -1,4 +1,5 @@
 //url-loader使用的时候加载图片需要依赖file-loader
+//增加string-loader,引入html片段
 module.exports = {
   entry: './main.js',
   output: {
@@ -6,7 +7,8 @@ module.exports = {
   },
   module: {
     loaders:[
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+      { test: /\.html$/,exclude:/node_modules/,loader:'string' }
     ]
   }
 };
