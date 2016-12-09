7f8e56be10aeb25f797f293bd6bd738d949ae461
sea13502 <sea13502@163.com>
2016-12-09 12:02:17 +0800

    增加按需加载的

--- otherbrick01/main.jsx
+++ otherbrick01/main.jsx
@@ -4,6 +4,11 @@ const ReactDOM = require('react-dom');
 //可以尝试些别的结构赋值
 let { a } = { a:123 };
 
+require.ensure(['./a'],function(){
+	var a = require('./a');
+	console.log(a);
+});
+
 ReactDOM.render(
   <h1>Hello, world!{a}</h1>,
   document.querySelector('#wrapper')
